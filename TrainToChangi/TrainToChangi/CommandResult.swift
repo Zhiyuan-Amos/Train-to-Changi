89919506eca3a212bc8696bf750344f3169d108c
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2017-03-15 22:17:26 +0800

    refactor Error into ExecutionError

--- TrainToChangi/TrainToChangi/CommandResult.swift
+++ TrainToChangi/TrainToChangi/CommandResult.swift
@@ -4,29 +4,12 @@
 //
 
 struct CommandResult {
-    let errorMessage: Error?
+    let errorMessage: ExecutionError?
     var isSuccessful: Bool {
         return errorMessage == nil
     }
 
-    init(errorMessage: Error? = nil) {
+    init(errorMessage: ExecutionError? = nil) {
         self.errorMessage = errorMessage
-    }
-
-    /**
-     An enum of errors that can be thrown from ModelManager
-     */
-    enum Error {
-        // Thrown when there are no inbox items remaining
-        case emptyInbox
-
-        // Thrown when person has no value
-        case emptyPersonValue
-
-        // Thrown when memory location to be accessed has no value
-        case emptyMemoryLocation
-
-        // Thrown when outbox value is wrong
-        case wrongOutboxValue
     }
 }
