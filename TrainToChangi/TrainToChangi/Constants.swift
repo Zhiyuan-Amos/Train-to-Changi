7459d8c4660d4d6b6250eb5713dc545909dfcd41
zhongwei zhang <zhangzhongwei.sg@gmail.com>
2017-03-18 11:21:19 +0800

    refactor

--- TrainToChangi/TrainToChangi/Constants.swift
+++ TrainToChangi/TrainToChangi/Constants.swift
@@ -1,4 +1,36 @@
+import UIKit
+
 struct Constants {
     // Struct is not meant to be initialised
     private init() {}
+
+    struct Dimensions {
+        static let bounds = UIScreen.main.bounds
+        static let width = bounds.width
+        static let height = bounds.height
+    }
+
+    struct Player {
+        static let size = CGSize(width: 80, height: 80)
+        static let position = CGPoint(x: Dimensions.width / 2, y: Dimensions.height / 2)
+    }
+
+    struct Inbox {
+        static let size = CGSize(width: 100, height: 500)
+        static let position = CGPoint(x: size.width / 2, y: size.height / 2)
+        static let color = UIColor.black
+        static let goto = CGPoint(x: size.width, y: size.height)
+    }
+
+    struct Outbox {
+        static let size = CGSize(width: 100, height: 500)
+        static let position = CGPoint(x: Dimensions.width - size.width / 2,
+                                      y: Dimensions.height - size.height / 2)
+        static let color = UIColor.black
+        static let goto = CGPoint(x: Dimensions.width - size.width, y: Dimensions.height - size.height)
+    }
+
+    struct NotificationNames {
+        static let movePersonInScene = Notification.Name(rawValue: "movePersonInScene")
+    }
 }
--- TrainToChangi/TrainToChangi/GameScene.swift
+++ TrainToChangi/TrainToChangi/GameScene.swift
@@ -11,21 +11,25 @@ import GameplayKit
 
 // Stores the location that can be reached by player sprite.
 // Each location is CGPoint stored as a String "{x, y}".
-enum StationLocation: CGPoint {
-    // TODO: positions calculation
-    case inbox = "{100, 0}"
-    case outbox = "{800, 800}"
+enum WalkDestination {
+    case inbox, outbox
+
+    var point: CGPoint {
+        switch self {
+        case .inbox:
+            return Constants.Inbox.goto
+        case .outbox:
+            return Constants.Outbox.goto
+        }
+    }
 }
 
 class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "player") // TODO: asset update
 
-    // TODO: refactor to constants
-    private let inbox = SKSpriteNode(color: .black, size: CGSize(width: 100, height: 500))
-    private let outbox = SKSpriteNode(color: .black, size: CGSize(width: 100, height: 500))
-    private let notificationName = Notification.Name(rawValue: "movePersonInScene")
+    private let inbox = SKSpriteNode()
+    private let outbox = SKSpriteNode()
     private let moveDuration = TimeInterval(2)
-
 
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.white // TODO: asset update
@@ -34,62 +38,49 @@ class GameScene: SKScene {
         initNotification()
     }
 
-    // TODO: positions calculation
     private func initPlayer() {
-        player.size = CGSize(width: 100, height: 100)
-        player.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        player.size = Constants.Player.size
+        player.position = Constants.Player.position
         addChild(player)
     }
 
     private func initStationElements() {
-        inbox.position = CGPoint(x: 0, y: 0)
-        outbox.position = CGPoint(x: size.width - 100, y: size.height - 500)
+        inbox.size = Constants.Inbox.size
+        inbox.color = Constants.Inbox.color
+        inbox.position = Constants.Inbox.position
+        outbox.size = Constants.Outbox.size
+        outbox.color = Constants.Outbox.color
+        outbox.position = Constants.Outbox.position
+
         addChild(inbox)
         addChild(outbox)
     }
 
     private func initNotification() {
-        let nc = NotificationCenter.default
-        let selector = #selector(catchNotification(notification:))
-        nc.addObserver(self, selector: selector, name: notificationName, object: nil)
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(catchNotification(notification:)),
+            name: Constants.NotificationNames.movePersonInScene, object: nil)
     }
 
     // Receive notification to control the game scene. Responds accordingly.
-    // notification must contains `userInfo` with "location" defined
+    // notification must contains `userInfo` with "destination" defined
     func catchNotification(notification: Notification) {
         guard let userInfo = notification.userInfo else {
             print("[GameScene:catchNotification] No userInfo found in notification")
             return
         }
 
-        guard let location = userInfo["location"] as? StationLocation else {
-            print("[GameScene:catchNotification] Unable to find location in userInfo")
+        guard let destination = userInfo["destination"] as? WalkDestination else {
+            print("[GameScene:catchNotification] Unable to find destination in userInfo")
             return
         }
 
-        move(to: location)
+        move(to: destination)
     }
 
-    // Move the player to a StationLocation
-    private func move(to location: StationLocation) {
-        let moveAction = SKAction.move(to: location.rawValue, duration: moveDuration)
+    // Move the player to a WalkDestination
+    private func move(to destination: WalkDestination) {
+        let moveAction = SKAction.move(to: destination.point, duration: moveDuration)
         player.run(moveAction)
-    }
-}
-
-// Extends CGPoint so that it's convertible from Sgring literal,
-// so CGPoint can be used as a raw type for enum
-extension CGPoint: ExpressibleByStringLiteral {
-
-    public init(stringLiteral value: StringLiteralType) {
-        self = CGPointFromString(value)
-    }
-
-    public init(extendedGraphemeClusterLiteral value: StringLiteralType) {
-        self = CGPointFromString(value)
-    }
-
-    public init(unicodeScalarLiteral value: StringLiteralType) {
-        self = CGPointFromString(value)
     }
 }
