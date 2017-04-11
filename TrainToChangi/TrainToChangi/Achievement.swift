2661609d30f047f6628622f6ae03815148c420fc
Yong Lin Han <lhlhyong@gmail.com>
2017-04-12 03:40:22 +0800

    Remove unneeded protocol conformance to NSCoding

--- TrainToChangi/TrainToChangi/Achievement.swift
+++ TrainToChangi/TrainToChangi/Achievement.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class Achievement: NSObject, NSCoding {
+class Achievement {
     let name: AchievementsEnum
     var isUnlocked: Bool
 
@@ -10,19 +10,4 @@ class Achievement: NSObject, NSCoding {
         self.isUnlocked = isUnlocked
     }
 
-    required init?(coder aDecoder: NSCoder) {
-        guard let rawValue = aDecoder.decodeObject(forKey: "name") as? String,
-            let name = AchievementsEnum(rawValue: rawValue) else {
-                fatalError("Data was not encoded rightly.")
-        }
-
-        self.name = name
-        self.isUnlocked = aDecoder.decodeBool(forKey: "isUnlocked")
-        super.init()
-    }
-
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(name.rawValue, forKey: "name")
-        aCoder.encode(isUnlocked, forKey: "isUnlocked")
-    }
 }
