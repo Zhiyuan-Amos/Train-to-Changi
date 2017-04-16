86b5ca4de31009250d53f352b37352297045f5ff
Yong Lin Han <lhlhyong@gmail.com>
2017-04-16 17:32:14 +0800

    Add achievements view

---
+++ TrainToChangi/TrainToChangi/AchievementsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AchievementsViewController.swift
+//  TrainToChangi
+//
+//  Created by Yong Lin Han on 16/4/17.
+//  Copyright © 2017 nus.cs3217.a0139655u. All rights reserved.
+//
+
+import UIKit
+
+class AchievementsViewController: UIViewController {
+
+    @IBOutlet var achievementsTableView: UITableView!
+    fileprivate(set) var achievements: [Achievement] = []
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        achievementsTableView.dataSource = self
+        achievementsTableView.delegate = self
+        achievementsTableView.layoutIfNeeded()
+
+        guard let userId = AuthService.instance.currentUserId else {
+            fatalError("User must be logged in!")
+        }
+        DataService.instance.loadUnlockedAchievements(userId: userId,
+                                                      loadUnlockedAchievementsDelegate: self)
+    }
+
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension AchievementsViewController: DataServiceLoadUnlockedAchievementsDelegate {
+    func load(unlockedAchievements: [String]) {
+        for unlockedAchievementStr in unlockedAchievements {
+            guard let achievementsEnum = AchievementsEnum(rawValue: unlockedAchievementStr) else {
+                fatalError("Cannot be initalised, wrong string value.")
+            }
+            achievements.append(Achievement(name: achievementsEnum, isUnlocked: true))
+        }
+        achievementsTableView.reloadData()
+    }
+}
--- TrainToChangi/TrainToChangi/TableViewExtensions.swift
+++ TrainToChangi/TrainToChangi/TableViewExtensions.swift
@@ -31,3 +31,35 @@ extension EndGameViewController: UITableViewDelegate {
         return 30
     }
 }
+
+extension AchievementsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "achievement"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier,
+                                                       for: indexPath) as? AchievementCell else {
+                                                        fatalError("Cell not assigned the proper view subclass!")
+        }
+        cell.setText(text: achievements[indexPath.item].name.toAchievementName())
+        cell.setImage(image: UIImage(named: achievements[indexPath.item].name.toImagePath())!)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(achievements.count)
+        return achievements.count
+    }
+}
+
+extension AchievementsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "You have unlocked these achievements:"
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+}
